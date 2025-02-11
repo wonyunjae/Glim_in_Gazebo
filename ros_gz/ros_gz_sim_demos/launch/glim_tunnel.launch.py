a4ceeb54eb05f5dc836d6d7821be59dbd9b44460
wynz <wyj5578@koreatech.ac.kr>
2025-02-11 17:35:01 +0900

    redefine tf, topics for glim

--- ros_gz/ros_gz_sim_demos/launch/glim_tunnel.launch.py
+++ ros_gz/ros_gz_sim_demos/launch/glim_tunnel.launch.py
@@ -52,51 +52,31 @@ def generate_launch_description():
         package='ros_gz_bridge',
         executable='parameter_bridge',
         arguments=[
-            '/lidar@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
             '/lidar/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked',
-            '/lidar/points/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked',
             'imu@sensor_msgs/msg/Imu@gz.msgs.IMU',
             '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
-            '/model/vehicle/pose@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V',
             '/model/vehicle/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry',
-            '/model/vehicle/imu_tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V'
+            '/model/vehicle/tf/@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V',
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+            
         ],
         remappings=[
-            ('/model/vehicle/pose', '/tf'),
-            ('/model/vehicle/odometry', '/odom')
+            ('/model/vehicle/odometry', '/odom'),
+            ('/model/vehicle/tf', '/tf'),
         ],
         parameters=[{
             'use_sim_time': True,
             'qos_overrides./lidar/points.publisher.reliability': 'reliable',
-            'qos_overrides./lidar/points.publisher.durability': 'volatile',
-            'qos_overrides./lidar/points.publisher.history': 'keep_last',
-            'qos_overrides./lidar/points.publisher.depth': 5,
             'qos_overrides.imu.publisher.reliability': 'reliable',
-            'qos_overrides.imu.publisher.durability': 'volatile',
-            'qos_overrides.imu.publisher.history': 'keep_last',
-            'qos_overrides.imu.publisher.depth': 5
+
         }],
         output='screen'
     )
     
-    imu_tf = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='imu_tf_publisher',
-        arguments=['0', '0', '0', '0', '0', '0', 'vehicle/base_link', 'vehicle/base_link/imu_sensor']
-    )
-    
-     # LiDAR TF publisher 추가
-    lidar_tf = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='lidar_tf_publisher',
-        arguments=['0.55', '0', '0.45', '0', '0', '0', 'vehicle/base_link', 'vehicle/base_link/lidar']
-    )
+
 
     return LaunchDescription([
         gz_sim,
         bridge,
-        imu_tf,
-        lidar_tf
+
     ])
